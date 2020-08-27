b942bed3c74882b407b53ab4154f4c23203f28ba
alhazen <mdzaidgr8@gmail.com>
2020-08-27 17:14:17 +0400

    Enable Caching on Loading Static Data

--- client/src/store/statics.js
+++ client/src/store/statics.js
@@ -1,19 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { apiCallBegan } from './api'
+import moment from 'moment'
 
 const { actions, reducer } = createSlice({
 	name: 'dropdowns',
 	initialState: {
 		cities: [],
 		visas: [],
+		loading: false,
+		lastFetch: null,
 	},
 
 	reducers: {
+		dataRequested: (state, action) => {
+			state.loading = true
+		},
+		dataRequestFailed: (state, action) => {
+			state.loading = false
+		},
 		citiesReceived: (state, action) => {
 			state.cities = action.payload
+			state.loading = false
+			state.lastFetch = Date.now()
 		},
 		visasReceived: (state, action) => {
 			state.visas = action.payload
+			state.loading = false
+			state.lastFetch = Date.now()
 		},
 	},
 })
@@ -23,6 +36,11 @@ export default reducer
 const { citiesReceived, visasReceived } = actions
 
 export const loadCities = () => (dispatch, getState) => {
+	const { lastFetch } = getState().statics
+	const diffInMinutes = moment().diff(moment(lastFetch), 'days')
+
+	if (diffInMinutes < 10) return
+
 	return dispatch(
 		apiCallBegan({
 			url: 'cities',
@@ -32,6 +50,11 @@ export const loadCities = () => (dispatch, getState) => {
 }
 
 export const loadVisas = () => (dispatch, getState) => {
+	const { lastFetch } = getState().statics
+	const diffInMinutes = moment().diff(moment(lastFetch), 'days')
+
+	if (diffInMinutes < 10) return
+
 	return dispatch(
 		apiCallBegan({
 			url: 'visas',
